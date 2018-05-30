3c4fb9ae3c44111288452095fb8d76c1eb8a16bf
Minh Duc <J3lackfire@gmail.com>
2018-05-31 00:55:40 +0300

    Finish dropbox handler. It can download file and save to local drive. If the file is large, it will take a while. If there are a lot of file, not all of them will be copied. Need to fix.

--- app.js
+++ app.js
@@ -4,16 +4,40 @@
 const express = require('express');
 const app = express();
 
+const dropboxHandler = require('./src/dropboxHandler')
+
 /*dependencies:
     express - basic to set up a server
     dropbox - for dropbox
     isometric-fetch - dropbox advice using this
+    //both of this might not be neccessary if I just cached the files download to memory somehow
     mkdirp - to create a directory to save file and stuffs //only to test on local though
+    fs - file system, to save file and stuffs
  */
 
-app.get('/', function(req, res){
-    res.send("Hello world!");
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World!'
+    });
 });
+
+app.get('/dropboxHandler', (req, res) => {
+    dropboxHandler.initDropboxHandler(
+        'KU1I6ilkxrAAAAAAAAAADmph3aEctjmw5LrRrAxHBeBsLabN0w2rN2j8hVlt84NA',
+        (_error, _message) => {
+            if (_error) {
+                res.json({
+                    message: _message,
+                    error: _error
+                })
+            } else {
+                res.json({
+                    message: _message,
+                    error: 'No error'
+                })
+            }
+        })
+})
 
 app.listen(3003);
 console.log('Express app listening on port 3003')
--- src/dropboxHandler.js
+++ src/dropboxHandler.js
@@ -2,6 +2,8 @@
  * Created by Le Pham Minh Duc on 29-May-18.
  */
 require('isomorphic-fetch'); // or another library of choice.
+let fs = require('fs')
+let mkdirp = require('mkdirp');
 let Dropbox = require('dropbox').Dropbox;
 
 let myAccessToken = 'KU1I6ilkxrAAAAAAAAAADmph3aEctjmw5LrRrAxHBeBsLabN0w2rN2j8hVlt84NA';
@@ -14,29 +16,19 @@ let baseParam = {
     limit: 50
 }
 
-let downloadFileParam = {
-
-}
-
 let allFolders = []
 let allFiles = []
 
 let folderTag = 'folder'
 let fileTag = 'file'
 
-function isEntryFolder(entry) {
-    return entry['.tag'] == folderTag
-}
+function isEntryFolder(entry) { return entry['.tag'] == folderTag }
 
-function isEntryFile(entry) {
-    return entry['.tag'] == fileTag
-}
+function isEntryFile(entry) { return entry['.tag'] == fileTag }
 
-let onErrorReceived = (error) => {
-    console.log(error)
-}
+let onErrorReceived = (error) => { console.log(error) }
 
-let onServerResponseReceived = (res) => {
+let onServerResponseReceived = (res, callback) => {
     console.log('onServerResponseReceived ')
     for (let i = 0; i < res.entries.length; i ++) {
         if (isEntryFolder(res.entries[i])) {
@@ -49,33 +41,86 @@ let onServerResponseReceived = (res) => {
     }
     if (res.has_more) {
         dbx.filesListFolderContinue( { cursor:res.cursor } )
-            .then(onServerResponseReceived)
+            .then((res_2) => {
+                onServerResponseReceived(res_2, callback)
+            })
             .catch(onErrorReceived)
     } else {
-        onListingCompleted()
+        onListingCompleted(callback)
     }
 }
 
-function listAllFoldersAndFiles(_accessToken) {
+function getDownloadFileParam(name) { return {path:name} }
+
+function downloadFileSingle(path, callback) {
+    dbx.filesDownload(getDownloadFileParam(path))
+        .then((response) => {
+        saveFileToLocal(response.path_lower, response.fileBinary, callback)
+    }).catch(onErrorReceived)
+
+}
+
+function makeDirectory() {
+    for (let i = 0; i < allFolders.length; i ++) {
+        mkdirp('cache' + allFolders[i], (err) => {
+            if (err) {
+                console.error(err)
+            }
+            else {
+                console.log('Succesfully create a directory: ' + allFolders[i])
+            }
+        });
+    }
+}
+
+function saveFileToLocal(name, fileBinary, callback) {
+    fs.writeFile("cache/" + name, fileBinary, "binary", function(err) {
+        if(err) {
+            console.log('Error happend in saving file to local!');
+            callback(err)
+        } else {
+            console.log('file saved to local! ' + name);
+            callback(null)
+        }
+    });
+}
+
+function initDropboxHandler(_accessToken, callback) {
     allFolders = []
     allFiles = []
     new Dropbox({ accessToken: _accessToken })
     dbx.filesListFolder(baseParam)
-        .then(onServerResponseReceived)
+        .then((res) => {
+            onServerResponseReceived(res, callback)
+        })
         .catch(onErrorReceived)
 }
 
-function onListingCompleted() {
-    console.log('\n\non Listing Completed: All folder')
-    console.log(allFolders)
-    console.log('\nNumber of files')
-    console.log(allFiles)
+
+//this function is called when the dropbox handler init is complete
+function onListingCompleted(callback) {
+    console.log('Finish getting all the meta data from dropbox server')
+    console.log('Start making directory')
+    makeDirectory()
+    let totalNumberOfFiles = allFiles.length
+    let numberOfSuccess = 0;
+    for (let i = 0; i < allFiles.length; i ++) {
+        downloadFileSingle(allFiles[i], (error) => {
+            if (error) {
+                console.log('Error downloading the file. something is wrong!')
+                callback(error, 'Error downloading the file. something is wrong!')
+            } else {
+                numberOfSuccess ++;
+                console.log('File: -' + allFiles[i] + '- index: ' + numberOfSuccess + '/' + totalNumberOfFiles)
+                if (numberOfSuccess >= totalNumberOfFiles) {
+                    console.log('The backup process is completed')
+                    callback(null, 'The backup process is completed, ' + totalNumberOfFiles + ' files is copied')
+                }
+            }
+        })
+    }
 }
 
-dbx.filesDownload({
-    path: '/duc_dog.jpg'
-}).then(response => {
-    console.log(response)
-}).catch(onErrorReceived)
+// initDropboxHandler(myAccessToken)
 
-// listAllFoldersAndFiles(myAccessToken)
+module.exports.initDropboxHandler = initDropboxHandler
